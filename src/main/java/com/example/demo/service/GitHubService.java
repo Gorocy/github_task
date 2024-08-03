5d8b64a9943b69ab85d65054d7ee73d6b98b0266
goracy <jakubgoracy@student.agh.edu.pl>
2024-08-03 23:14:51 +0200

    service for controller

---
+++ src/main/java/com/example/demo/service/GitHubService.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.GitHubUserNotFoundException;
+import com.example.demo.model.Branch;
+import com.example.demo.model.Repository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GitHubService {
+
+    private final RestTemplate restTemplate;
+    private final String githubApiUrl;
+
+    public GitHubService(@Value("${github.api.url}") String githubApiUrl) {
+        this.restTemplate = new RestTemplate();
+        this.githubApiUrl = githubApiUrl;
+    }
+
+    public List<Repository> listNonForkRepositories(String username) {
+        try {
+            String url = String.format("%s/users/%s/repos", githubApiUrl, username);
+            Repository[] repositories = restTemplate.getForObject(url, Repository[].class);
+
+            if (repositories == null) {
+                throw new GitHubUserNotFoundException(username);
+            }
+
+            return Arrays.stream(repositories)
+                    .filter(repo -> !repo.isFork())
+                    .map(repo -> {
+                        List<Branch> branches = fetchBranches(repo);
+                        repo.setBranches(branches);
+                        return repo;
+                    })
+                    .collect(Collectors.toList());
+
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new GitHubUserNotFoundException(username);
+        }
+    }
+
+    private List<Branch> fetchBranches(Repository repository) {
+        String url = repository.getBranchesUrl().replace("{/branch}", "");
+        Branch[] branches = restTemplate.getForObject(url, Branch[].class);
+        return Arrays.asList(branches);
+    }
+}
